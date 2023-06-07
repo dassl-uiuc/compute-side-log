36cc0bb25a796cf8851c31ccfd448e782306a69c
Xuhao Luo <rossihox@gmail.com>
2023-06-07 15:16:52 -0500

    fetch glog at compile time

--- install-deps.sh
+++ install-deps.sh
@@ -6,17 +6,9 @@ sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
 sudo apt-get update
 sudo apt-get install cmake libunwind-dev libgflags-dev openjdk-11-jre-headless -y
 
-# install glog
-git clone https://github.com/google/glog.git ../glog
-cd ../glog
-git checkout v0.6.0
-cmake -S . -B build
-cmake --build build
-sudo cmake --build build --target install
-
 # install zookeeper
 sudo apt install maven libcppunit-dev -y
-cd ../zookeeper/zookeeper-jute && mvn compile && cd ..
+cd ./zookeeper/zookeeper-jute && mvn compile && cd ..
 cd zookeeper-client/zookeeper-client-c
 autoreconf -if
 ./configure --with-syncapi
